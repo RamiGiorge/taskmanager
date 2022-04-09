028354cd9cfdcd698087fd3906327f61332f7198
Rome <rami.giorge@gmail.com>
2022-04-09 13:51:01 +0300

    Added Image Upload Functionality

--- src/components/SideMenu/SideMenu.js
+++ src/components/SideMenu/SideMenu.js
@@ -1,10 +1,12 @@
 import { FaChevronLeft, FaBookmark, FaClock, FaThList } from 'react-icons/fa'
 import { useSelector } from 'react-redux'
 import { tasksState } from '../../features/tasksSlice'
-import moero from "../../assets/moero.jpeg";
+import user from "../../assets/user.jpeg";
+import { useEffect, useState } from 'react';
 
 const SideMenu = ({ toggleMenu }) => {
     const { tasks } = useSelector(tasksState)
+    const [img, setImg] = useState(null)
 
     const getScore = () => {
         if (tasks.length) {
@@ -17,6 +19,23 @@ const SideMenu = ({ toggleMenu }) => {
         return (<span>Add tasks to show consistency</span>)
     }
 
+    useEffect(() => {
+        const storedImg = localStorage.getItem('img')
+        storedImg && setImg(storedImg)
+    }, [])
+
+    const handleUpload = (e) => {
+        const img = e.target.files[0];
+        if (img) {
+            const reader = new FileReader()
+            reader.addEventListener('load', (e) => {
+                setImg(e.target.result);
+                localStorage.setItem('img', e.target.result)
+            })
+            reader.readAsDataURL(img)
+        }
+    };
+
     return (
         <aside className='side-menu'>
             <div className="inner-side">
@@ -24,8 +43,11 @@ const SideMenu = ({ toggleMenu }) => {
                     <FaChevronLeft />
                 </button>
                 <div className="profile-img-container">
-                    <img src={moero} alt="profile" className='profile-img' />
+                    <img src={img ? img : user} alt="profile" className='profile-img' />
                 </div>
+
+                <input type="file" accept="image/png, image/jpg" onChange={handleUpload} className='upload-input' />
+
                 <h1>Rami George</h1>
                 <ul className="submenu">
                     <li className='pointer'>
